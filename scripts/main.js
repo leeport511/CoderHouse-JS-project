3138e35d410581f4f57e11f8dbed33f83d7691b0
leeport511 <leonardoandres.portillo@gmail.com>
2023-06-14 22:14:49 -0300

    terminado primer entrega

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+let index = 0;
+let cargaPaises = prompt(
+  "Cargue todos los nombres de los paises que quiera, salga del programa escribiendo ESC \n\n"
+);
+let listadoPaises = "";
+
+const cargarPaises = (paises) => {
+  while (paises !== "ESC") {
+    index++;
+
+    if (paises === "ESC") {
+      break;
+    }
+
+    listadoPaises += `${index}. ${paises}\n`;
+    paises = prompt(
+      "Cargue todos los nombres de los paises que quiera, salga del programa escribiendo ESC\n\n"
+    );
+    console.log(listadoPaises);
+  }
+};
+
+cargarPaises(cargaPaises);
+
+alert(`Esta es la lista de paises que cargo:\n\n${listadoPaises}`);
+
