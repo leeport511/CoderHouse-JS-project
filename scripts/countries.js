b850b61bfab18943b1de5591b1463d8f6eb4138e
leeport511 <leonardoandres.portillo@gmail.com>
2023-07-12 19:56:49 -0300

    add a constructor file and sort the array in alphabetichal order

--- scripts/countries.js
+++ scripts/countries.js
@@ -1,29 +1,42 @@
-import countryData from "../constants/countryData.mjs";
+import countries from "../scripts/constructor.js";
 
-console.log(countryData);
+console.log(countries);
 
-for (const country of countryData) {
-    console.log(country.flags.png);
-    
-}
+countries.sort((a, b) => {
+    let countryA = a.name.toLowerCase();
+    let countryB = b.name.toLowerCase();
+
+    if(countryA < countryB) {
+        return -1;
+    }
+    if(countryA > countryB) {
+        return 1;
+    }
+    return 0;
+
+});
+
+console.log(countries);
+
+
+// for (const country of countries) {
+//   console.log(country.name);
+// }
+
 
 const flagCard = () => {
-
-    for (const country of countryData) {
-        
-        let flagContainer = document.querySelector(".flags-container");
-        let flagCard = document.createElement("DIV");
-        flagCard.className = "flag-card";
-        flagCard.innerHTML = `<img src="${country.flags.png}" alt="Test Flag"></img>
+  for (const country of countries) {
+    let flagContainer = document.querySelector(".flags-container");
+    let flagCard = document.createElement("DIV");
+    flagCard.className = "flag-card";
+    flagCard.innerHTML = `<img src="${country.flag}" alt="Test Flag"></img>
                                 <div class="flag-card-info">
-                                    <h4>${country.name.common}</h4>
+                                    <h4>${country.name}</h4>
                                     <button><img src="../assets/images/plus-icon.svg" alt="more..."></button>
                                 </div>`;
-    
-        flagContainer.append(flagCard);
-        
-    }
 
+    flagContainer.append(flagCard);
+  }
 };
 
 flagCard();
