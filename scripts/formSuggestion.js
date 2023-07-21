f89cab86ecbe278ce6a7e0d8ea0cf76bfdc13cd2
leeport511 <leonardoandres.portillo@gmail.com>
2023-07-21 18:24:45 -0300

    Fixed modal responsive view

--- scripts/formSuggestion.js
+++ scripts/formSuggestion.js
@@ -2,22 +2,40 @@
 
 let form = document.querySelector(".suggestions-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  let name = e.target[0].value;
+  let suggestion = e.target[1].value;
 
-    let name = e.target[0].value;
-    let suggestion = e.target[1].value;
-
-    if (name === '' || suggestion === '') {
-        
-        //msj de error por sweetAlert en futuro
-        alert('One of the fields are empty');
-        return;
-    } else {
-
-        //msj de success por sweetAlert en futuro
-        alert(`Thank you for the suggestion ${name}, we'll consider it`)
-    }
-
-})
+  if (name === "" || suggestion === "") {
+    Swal.fire({
+      icon: "error",
+      iconColor: '#bbe1fa',
+      title: "Error",
+      text: "One of the fields are empty!",
+      width: '24em',
+      padding: '5px',
+      buttonsStyling: false,
+      customClass: {
+        title: "swal2-title2",
+        htmlContainer: 'swal2-paragrahp',
+      }
+    });
+    return;
+  } else {
+    Swal.fire({
+      icon: "success",
+      iconColor: '#bbe1fa',
+      title: "Well Done",
+      text: `Thank you for the suggestion ${name}, we'll consider it!`,
+      width: '24em',
+      padding: '5px',
+      buttonsStyling: false,
+      customClass: {
+        title: "swal2-title2",
+        htmlContainer: 'swal2-paragrahp',
+      },
+    });
+  }
+});
